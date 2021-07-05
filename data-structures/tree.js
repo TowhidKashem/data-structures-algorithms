3f9b2ba50e8d3a84ac8560e6bb625fb0fbe6a65b
TowhidKashem <towhid.kashem@bit.ly>
2021-07-05 11:40:14 -0400

    work

--- data-structures/tree.js
+++ data-structures/tree.js
@@ -1,3 +1,5 @@
+/*------------------------------------------------ Tree ------------------------------------------------*/
+
 /*
 
     [20]          [40]          [-15]
@@ -75,8 +77,12 @@ class Tree {
     }
   }
 
-  // Depth first traversal is the exact same algorithm as breadth first
-  // the only difference is instead of pushing the children to the end of the array you add it to the begining
+  /*
+    - Also from left to right
+    - Go all the way down then back up to the closest parent and back down each branch
+    - Depth first traversal is the exact same algorithm as breadth first
+    - the only difference is instead of pushing the children to the end of the array you add it to the begining (unshift() vs push())
+  */
   depthFirstTraversal(fn) {
     const nodes = [this.root];
 
@@ -115,3 +121,96 @@ function getLevelWidth(root) {
 
   return nodesCount;
 }
+
+/*------------------------------------------------ Binary Tree ------------------------------------------------*/
+
+// Same as any other tree except a parent can only have 2 child nodes
+// the node to the bottom left is called the left node and the one to the bottom right is called the right node
+
+/*------------------------------------------------ Binary Search Tree ------------------------------------------------*/
+
+// Same as a binary tree except the value of the left node must be smaller than the parent and the value of the
+// right node must be larger than the parent
+
+// Problem: insert a node at the appropriate spot in a binary search tree (recursive solution):
+//
+//   test("Node can insert correctly", () => {
+//     const node = new Node(10);
+//     node.insert(5);
+//     node.insert(15);
+//     node.insert(17);
+
+//     expect(node.left.data).toEqual(5);
+//     expect(node.right.data).toEqual(15);
+//     expect(node.right.right.data).toEqual(17);
+//   });
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    // If val is smaller and there is a child node on the left keep digging deeper
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    }
+    // If no left child just add one
+    else if (data < this.data) {
+      this.left = new Node(data);
+    }
+    // If val is larger and there is a child node on the right keep digging deeper
+    else if (data > this.data && this.right) {
+      this.right.insert(data);
+    }
+    // If no right child just add one
+    else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+}
+
+// Problem: check to see if a node exists in a binary search tree (recursive solution)
+// This solution is efficient because we only need to go down one side of the tree or the other and not traverse the entire tree
+// so we use a recursive solution instead of the usual Breadth first or Depth first traversal patterns:
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  contains(data) {
+    // If there's a match we return the node
+    if (this.data === data) {
+      return this;
+    }
+
+    // Keep digging left
+    if (data < this.data && this.left) {
+      return this.left.contains(data);
+    }
+    // Keep digging right
+    else if (data > this.data && this.right) {
+      return this.right.contains(data);
+    }
+
+    // If node doesn't exist previous checks will be missed and we return null
+    return null;
+  }
+}
+
+/*
+
+Notes:
+
+  2 common tree questions are:
+    - Given a value insert it at the appropriate spot on the tree
+    - Validate the well formedness of a binary tree
+
+  Recursion is often used to insert nodes in a binary search tree
+
+*/
