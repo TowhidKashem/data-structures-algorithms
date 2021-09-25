4f740873528a071265508a46c7be09277e30a1f3
Towhid Kashem <tkashem@gmail.com>
2021-09-24 20:31:14 -0400

    work

--- graph/graph.js
+++ graph/graph.js
@@ -96,34 +96,22 @@ const graph = [
 
 class Graph {
   constructor() {
+    this.adjacencyList = {};
     this.numberOfNodes = 0;
-    this.adjacentList = {};
   }
   addVertex(node) {
-    this.adjacentList[node] = [];
+    this.adjacencyList[node] = [];
     this.numberOfNodes++;
   }
   addEdge(node1, node2) {
     // Undirected Graph
-    this.adjacentList[node1].push(node2);
-    this.adjacentList[node2].push(node1);
-  }
-  // For debugging
-  showConnections() {
-    const allNodes = Object.keys(this.adjacentList);
-    for (let node of allNodes) {
-      let nodeConnections = this.adjacentList[node];
-      let connections = "";
-      let vertex;
-      for (vertex of nodeConnections) {
-        connections += vertex + " ";
-      }
-      console.log(node + "-->" + connections);
-    }
+    this.adjacencyList[node1].push(node2);
+    this.adjacencyList[node2].push(node1);
   }
 }
 
 const myGraph = new Graph();
+
 myGraph.addVertex("0");
 myGraph.addVertex("1");
 myGraph.addVertex("2");
@@ -131,6 +119,7 @@ myGraph.addVertex("3");
 myGraph.addVertex("4");
 myGraph.addVertex("5");
 myGraph.addVertex("6");
+
 myGraph.addEdge("3", "1");
 myGraph.addEdge("3", "4");
 myGraph.addEdge("4", "2");
@@ -140,12 +129,19 @@ myGraph.addEdge("1", "0");
 myGraph.addEdge("0", "2");
 myGraph.addEdge("6", "5");
 
-myGraph.showConnections();
+console.log(myGraph);
 
-// 0-->1 2
-// 1-->3 2 0
-// 2-->4 1 0
-// 3-->1 4
-// 4-->3 2 5
-// 5-->4 6
-// 6-->5
+/*
+Graph {
+  adjacencyList: {
+    '0': [ '1', '2' ],
+    '1': [ '3', '2', '0' ],
+    '2': [ '4', '1', '0' ],
+    '3': [ '1', '4' ],
+    '4': [ '3', '2', '5' ],
+    '5': [ '4', '6' ],
+    '6': [ '5' ]
+  },
+  numberOfNodes: 7
+}
+*/
