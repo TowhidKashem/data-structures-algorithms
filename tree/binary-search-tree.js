f53363e1db48ed331de2b05bdc1c018a8b34257d
Towhid Kashem <tkashem@gmail.com>
2021-12-24 11:14:49 -0600

    work

--- tree/binary-search-tree.js
+++ tree/binary-search-tree.js
@@ -1,42 +1,45 @@
 /*
-    Rules:
+    BST Rules:
         1. Has 0, 1, or 2 nodes
         2. Kept in a sorted order of some kind
-        3. All children to the left of a parent node are smaller than it's value and all children to the right are larger
+        3. All children to the left of a parent node are smaller than it's val and all children to the right are larger
 
     BST's are great for searching as opposed to an unsorted tree, since you don't need to visit each node, it's a devide and conquer approach
 
     Complexity: O(log n) for both insert and search on average and O(n) for worst case
+
+    Traversal:
+      Breadth = width of the tree / horizontal
+      Depth = height of the tree / vertical
 */
 
 class BinarySearchTree {
-  constructor(value) {
-    this.TreeNode = function (value) {
-      this.value = value;
+  constructor() {
+    this.root = null;
+    this.Node = function (val) {
+      this.val = val;
       this.left = null;
       this.right = null;
     };
-    this.root = value ? new this.TreeNode(value) : null;
   }
 
   insert(insertVal) {
-    const newNode = new this.TreeNode(insertVal);
+    const newNode = new this.Node(insertVal);
 
     if (!this.root) {
       this.root = newNode;
-      return this;
     }
 
-    function traverseTree(node) {
+    function traverse(node) {
       // Avoid inserting duplicates
-      if (insertVal === node.value) {
+      if (insertVal === node.val) {
         return;
       }
 
-      if (insertVal < node.value) {
+      if (insertVal < node.val) {
         // Keep going down left
         if (node.left) {
-          traverseTree(node.left);
+          traverse(node.left);
         } else {
           // Reached the end
           node.left = newNode;
@@ -44,7 +47,7 @@ class BinarySearchTree {
       } else {
         // Keep going down right
         if (node.right) {
-          traverseTree(node.right);
+          traverse(node.right);
         } else {
           // Reached the end
           node.right = newNode;
@@ -52,41 +55,18 @@ class BinarySearchTree {
       }
     }
 
-    traverseTree(this.root);
-
-    return this;
+    traverse(this.root);
   }
 
-  bfsRecursive() {
-    const nodes = [];
-
-    function traverseTree(node, level) {
-      if (nodes[level]) {
-        nodes[level] = (nodes[level] + node.value) / 2;
-      } else {
-        nodes[level] = node.value;
-      }
-
-      console.log(level + " -> ", node.value);
-
-      if (node.left) traverseTree(node.left, level + 1);
-      if (node.right) traverseTree(node.right, level + 1);
-    }
-
-    traverseTree(this.root, 0);
-
-    return nodes;
-  }
-
-  bfsIterative() {
+  // Iterative (recursive way isn't working/seems harder)
+  bfs() {
     const queue = [this.root];
     const nodes = [];
 
     while (queue.length) {
       const node = queue.shift();
 
-      nodes.push(node.value);
-      console.log(node.value);
+      nodes.push(node.val);
 
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
@@ -95,44 +75,83 @@ class BinarySearchTree {
     return nodes;
   }
 
-  dfsRecursive(type) {
-    const vals = [];
+  dfsPreOrder() {
+    const nodes = [];
 
-    switch (type) {
-      case "pre-order":
-        function traverse(node) {
-          vals.push(node.value);
+    function traverse(node) {
+      nodes.push(node.val);
 
-          if (node.left) traverse(node.left);
-          if (node.right) traverse(node.right);
-        }
-
-        traverse(this.root);
-        break;
-      case "post-order":
-        //
-        break;
-      case "in-order":
-        //
-        break;
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
     }
 
-    return vals;
+    traverse(this.root);
+
+    return nodes;
+  }
+
+  // Code here is exactly the same as `dfsPreOrder()`
+  // only difference is we push the value AFTER both recursive calls
+  dfsPostOrder() {
+    const nodes = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+
+      nodes.push(node.val);
+    }
+
+    traverse(this.root);
+
+    return nodes;
+  }
+
+  // Code here is exactly the same as `dfsPreOrder()`
+  // only difference is we push the value AFTER the traverse left recursive call
+  dfsInOrder() {
+    const nodes = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+
+      nodes.push(node.val);
+
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+
+    return nodes;
   }
 }
 
-const bst = new BinarySearchTree(50);
+const bst = new BinarySearchTree();
 
+bst.insert(50);
 bst.insert(40);
 bst.insert(60);
 bst.insert(55);
 bst.insert(70);
 bst.insert(65);
+bst.insert(30);
 
-// console.log(JSON.stringify(bst));
+// Visualized tree: https://i.imgur.com/lMSJeKl.png
 
-// console.log(bst.bfsRecursive());
-// console.log('----------------');
-// console.log(bst.bfsIterative());
+//console.log(JSON.stringify(bst));
 
-console.log(bst.dfsRecursive("pre-order"));
+// BFS: horizontal, level by level from left to right
+// https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/8344808
+//console.log(bst.bfs()); // [50, 40, 60, 30, 55, 70, 65]
+
+// DFS (Pre Order): vertical, go all the way down the left side, then up to the nearest parent, then down the right side and so on...
+// https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/11082104
+//console.log(bst.dfsPreOrder()); // [50, 40, 30, 60, 55, 70, 65]
+
+// DFS (Post Order): visit all children of a node before visiting the node itself
+// https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/11082110
+//console.log(bst.dfsPostOrder()); // [30, 40, 55, 65, 70, 60, 50]
+
+// DFS (In Order): go all the way down the left, then visit the node, then repeat on the right side
+// https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/11082114
+console.log(bst.dfsInOrder()); // [30, 40, 50, 55, 60, 65, 70]
