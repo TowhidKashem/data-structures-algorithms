f76e3d4a51bf72c9878d870fbf3a2b71ea39f144
Towhid Kashem <tkashem@gmail.com>
2022-01-14 19:44:40 -0600

    work

--- array/two-pointers.js
+++ array/two-pointers.js
@@ -1,4 +1,5 @@
 // Two pointer approach ususally makes sense on sorted data
+// Remove duplicates from an array in-place
 function getUniqueValues(arr) {
   if (arr.length === 0) return [];
 
@@ -6,15 +7,13 @@ function getUniqueValues(arr) {
   for (let j = 1; j < arr.length; j++) {
     if (arr[i] !== arr[j]) {
       i++;
-      arr[i] = arr[j]; // update in place
+      arr[i] = arr[j];
     }
   }
 
-  // arr = [-1, 1, 2, 3, 4, 5, 6, 7, 5, 6, 7, 7, 7];
-
-  return arr.slice(0, i + 1); // [-1, 1, 2, 3, 4, 5, 6, 7];
+  return arr.slice(0, i + 1);
 }
 
-// [-1,1,2,3,3,3,4,5,5,6,7,7,7]
-//             i j
-console.log(getUniqueValues([-1, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 7, 7]));
+//               i  j
+// [-1, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 7, 7]
+console.log(getUniqueValues([-1, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 7, 7])); // [-1, 1, 2, 3, 4, 5, 6, 7];
