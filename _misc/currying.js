3f51a436fac341aedb3d10d0dcf111c7eb49ecf1
TowhidKashem <tkashem@gmail.com>
2022-03-01 23:00:06 -0600

    work

--- _misc/currying.js
+++
@@ -1,9 +0,0 @@
-const greetSomeone = (greeting) => {
-  return (person) => {
-    return (endMark) => {
-      console.log(greeting + " " + person + endMark);
-    };
-  };
-};
-
-greetSomeone("Hello")("TK")("!"); // Hello TK!
